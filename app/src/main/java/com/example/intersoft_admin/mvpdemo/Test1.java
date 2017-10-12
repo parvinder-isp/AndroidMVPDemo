c58cc084ae178aa65a66ab5dfc462c928e284cb8
Parvinder Singh <parvinder.intersoft@gmail.com>
2017-10-12 13:03:26 +0530

    Added the Test1 File

---
+++ app/src/main/java/com/example/intersoft_admin/mvpdemo/Test1.java
@@ -0,0 +1,16 @@
+package com.example.intersoft_admin.mvpdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by intersoft-admin on 12/10/17.
+ */
+
+public class Test1 extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
