5c57a8836a3cab12b652d6d0dd79721f5ad795ad
Parvinder Singh <parvinder.intersoft@gmail.com>
2017-10-12 12:36:42 +0530

    Added the Test File

---
+++ app/src/main/java/com/example/intersoft_admin/mvpdemo/Test.java
@@ -0,0 +1,17 @@
+package com.example.intersoft_admin.mvpdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by intersoft-admin on 12/10/17.
+ */
+
+public class Test extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
